82c152b36308de5a059c35ed7873caa0c68f4f22
aalbino2 <andrea.albino@hotmail.it>
2024-11-18 19:13:47 +0100

    ruff linting

--- src/pdi_nomad_plugin/mbe/processes.py
+++ src/pdi_nomad_plugin/mbe/processes.py
@@ -1,7 +1,6 @@
 import json
 
 import numpy as np
-import plotly.graph_objects as go
 from nomad.config import config
 from nomad.datamodel.data import ArchiveSection, EntryData
 from nomad.datamodel.hdf5 import HDF5Reference
@@ -20,7 +19,7 @@ from nomad.datamodel.metainfo.basesections import (
     System,
     SystemComponent,
 )
-from nomad.datamodel.metainfo.plot import PlotlyFigure, PlotSection
+from nomad.datamodel.metainfo.plot import PlotSection
 from nomad.datamodel.metainfo.workflow import (
     Link,
 )
